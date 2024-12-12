253928f85feda6e77284185514f8399a8339c3e8
A00314438 <A00314438@mycambrian.ca>
2024-12-12 05:03:22 +0000

    final commit

---
+++ Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace NuGet_Package
+{
+    public class Class1
+    {
+        public void print()
+        {
+            Console.WriteLine("Parth Patel (A00314438)");
+        }
+
+    }
+}
+
